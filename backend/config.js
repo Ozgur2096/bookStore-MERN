2fe47c06bfec00b10d625004fa79bd2b86f06814
Ozgur Ozhan <ozgur2096@gmail.com>
2024-03-20 11:59:43 +0100

    database connected, models added

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://new-user:nVFp2BYJeJJTDqlm@cluster0.0w6ihvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  'mongodb+srv://new-user:nVFp2BYJeJJTDqlm@cluster0.0w6ihvn.mongodb.net/book_store?retryWrites=true&w=majority&appName=Cluster0';
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,42 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { PORT, mongoDBURL } from './config.js';
 
+import { Book } from './models/bookModels.js';
+
 const app = express();
+
+// middleware for parsing request body
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  console.log(req.url);
+  res.status(200).send('Hello World!');
+});
+
+// route for saving a new book
+app.post('/books', async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publicationYear
+    ) {
+      return response
+        .status(400)
+        .send('Please provide all the required fields');
+    }
+    const newBook = new Book({
+      title: request.body.title,
+      author: request.body.author,
+      publicationYear: request.body.publicationYear,
+    });
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ message: error.message });
+  }
+});
 
 mongoose
   .connect(mongoDBURL)
@@ -15,8 +50,3 @@ mongoose
   .catch(err => {
     console.log(err);
   });
-
-app.get('/', (req, res) => {
-  console.log(req.url);
-  res.status(200).send('Hello World!');
-});
