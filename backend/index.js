c2cb2fbdcb08d2a2b9299cbfc05327555231ec83
Ozgur Ozhan <ozgur2096@gmail.com>
2024-03-25 10:09:36 +0100

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,26 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { PORT, mongoDBURL } from './config.js';
+import cors from 'cors';
 
+import { PORT, mongoDBURL } from './config.js';
 import bookRoutes from './routes/books.js';
 
 const app = express();
 
 // middleware for parsing request body
 app.use(express.json());
+
+// middleware for handling CORS requests
+// option 1: allow all origins with default of cors
+// app.use(cors());
+// option 2: allow custom origins
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type',
+  })
+);
 
 app.use('/books', bookRoutes);
 
